1c8cfb73bfe76b73fa7a5ce4355e9ea30bd5ed9b
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2024-10-29 23:08:46 +0300

    refactor

--- GameOfLifeStrings/GameOfLifeStrings/Program.cs
+++ GameOfLifeStrings/GameOfLifeStrings/Program.cs
@@ -17,7 +17,7 @@ namespace GameOfLifeStrings
             var matrix = GetValidMatrix(str);
 
             var rowsCount = matrix.Length;
-            var columnsCount = matrix.First().Length;
+            var columnsCount = matrix[0].Length;
 
             var outputStringBuilder = new StringBuilder();
 
@@ -28,20 +28,20 @@ namespace GameOfLifeStrings
                     var targetCell = matrix[rowIndex][columnIndex];
                     var isAlive = targetCell == '1';
 
-                    var liveNeighbours = GetLiveNeighbours(matrix, rowIndex, columnIndex);
+                    var liveNeighbors = GetLiveNeighbors(matrix, rowIndex, columnIndex);
 
                     outputStringBuilder.Append(isAlive switch
                     {
-                        true when liveNeighbours is 2 or 3 => "1",
+                        true when liveNeighbors is 2 or 3 => "1",
                         true => "0",
-                        false when liveNeighbours is 3 => "1",
+                        false when liveNeighbors is 3 => "1",
                         _ => targetCell
                     });
                 }
 
                 if (rowIndex != rowsCount - 1)
                 {
-                    outputStringBuilder.Append("_");
+                    outputStringBuilder.Append('_');
                 }
             }
 
@@ -54,8 +54,7 @@ namespace GameOfLifeStrings
             {
                 throw new ArgumentNullException(nameof(str), "The input string can't be null or empty.");
             }
-
-            if (str.Distinct().Any(c => c != '0' && c != '1' && c != '_'))
+            if (Array.Exists(str.Distinct().ToArray(), c => c != '0' && c != '1' && c != '_'))
             {
                 throw new ArgumentException("The input string must contain only '1' or '0' or '_' symbols.", nameof(str));
             }
@@ -65,25 +64,20 @@ namespace GameOfLifeStrings
         {
             var separatedRows = str.Split('_');
 
-            if (separatedRows.Any(row => row.Length != separatedRows.First().Length))
+            if (!Array.TrueForAll(separatedRows, row => row.Length == separatedRows[0].Length))
             {
                 throw new ArgumentException("The input string must contain substrings of equal length.", nameof(str));
             }
 
-            return separatedRows.Select(s => s.ToArray()).ToArray();
+            return separatedRows.Select(s => s.ToCharArray()).ToArray();
         }
 
-        private static int GetLiveNeighbours(char[][] matrix, int targetRowIndex, int targetColumnIndex)
-        {
-            return GetNeighbours(matrix, targetRowIndex, targetColumnIndex).Count(x => x == '1');
-        }
-
-        private static string GetNeighbours(char[][] matrix, int targetRowIndex, int targetColumnIndex)
+        private static int GetLiveNeighbors(char[][] matrix, int targetRowIndex, int targetColumnIndex)
         {
             var rowsCount = matrix.Length;
             var columnsCount = matrix[0].Length;
 
-            var neighbours = new StringBuilder();
+            var liveNeighbors = 0;
 
             var (startRowIndex, endRowIndex, startColumnIndex, endColumnIndex) = GetMinMaxIndexes(rowsCount, columnsCount, targetRowIndex, targetColumnIndex);
 
@@ -91,14 +85,15 @@ namespace GameOfLifeStrings
             {
                 for (var columnIndex = startColumnIndex; columnIndex <= endColumnIndex; columnIndex++)
                 {
-                    if (rowIndex != targetRowIndex || columnIndex != targetColumnIndex)
+                    if ((rowIndex != targetRowIndex || columnIndex != targetColumnIndex)
+                        && matrix[rowIndex][columnIndex] == '1')
                     {
-                        neighbours.Append(matrix[rowIndex][columnIndex]);
+                        liveNeighbors++;
                     }
                 }
             }
 
-            return neighbours.ToString();
+            return liveNeighbors;
         }
 
         private static (int startRowIndex, int endRowIndex, int startColumnIndex, int endColumnIndex) GetMinMaxIndexes(
